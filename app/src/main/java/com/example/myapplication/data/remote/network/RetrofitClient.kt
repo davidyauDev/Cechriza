33d84ce83fa7a2a7da85543e53805f92608b4a99
David <daviddevyauri@gmail.com>
2025-11-25 14:39:49 -0500

    Update RetrofitClient base URL to use secure API endpoint

--- app/src/main/java/com/example/myapplication/data/remote/network/RetrofitClient.kt
+++ app/src/main/java/com/example/myapplication/data/remote/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://172.19.0.17/api/"
+    private const val BASE_URL = "https://api-asistencia.cechriza.com/api/"
 
     fun apiWithToken(tokenProvider: () -> String?): ApiService {
         val okHttp = OkHttpClient.Builder()
